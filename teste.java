b09020517762217fb734ea8f5d181a87cbd362dd
Gabriel Alves Fernandes <gabriel.fernandes@HOT-C02DX6P4MD6N.local>
2022-08-15 17:13:08 -0300

    Commit inicial

---
+++ teste.java
@@ -0,0 +1,26 @@
+package com.hotmart.capi.adapters.inbound;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hotmart.capi.application.core.domain.Event;
+import com.hotmart.capi.application.core.domain.EventResponse;
+import com.hotmart.capi.application.ports.in.EventInputPort;
+
+@RestController
+@RequestMapping(value = {"/sendEvent"})
+public class ReceiptEventRestAdapter {
+	
+	@Autowired
+	private  EventInputPort sendEventInputPort;
+
+    @PostMapping()
+    public EventResponse sendEvent(@RequestBody Event event){
+    	EventResponse returnEvent = sendEventInputPort.sendEvent(event);
+        return returnEvent;
+    }
+
+}
